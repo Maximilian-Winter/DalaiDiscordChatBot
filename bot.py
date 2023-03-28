04cb6fd081a69366233aa8c05ade6f01392793cd
Maximilian Winter <maximilian.winter.91@gmail.com>
2023-03-28 03:06:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ def create_prompt(instruction, input_data=None):
                 ### Response:
                 """
     else:
-        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+        return f"""You are a honest and helpful chat bot named 'BadMotherfucker'. Below is an instruction that describes a task. Write a response that appropriately completes the request.
                 ### Instruction:
                 {instruction}
                 ### Response:
@@ -71,7 +71,7 @@ def create_prompt(instruction, input_data=None):
 #except (FileNotFoundError, json.JSONDecodeError):
 config = {
     "seed": -1,
-    "threads": 16,
+    "threads": 24,
     "n_predict": 800,
     "top_k": 40,
     "top_p": 0.9,
@@ -111,6 +111,31 @@ client = MyClient(intents=intents)
 intents.messages = True
 output_text = ""
 is_generating_chat_result = False
+
+
+def to_utf8_compatible(input_string):
+    if isinstance(input_string, str):
+        return input_string.encode('utf-8', errors='replace').decode('utf-8')
+    else:
+        raise TypeError("Input must be a string")
+
+
+def extract_text(input_str):
+    end_idx = input_str.find("[end of text]")
+    if end_idx == -1:
+        return input_str
+    else:
+        return input_str[:end_idx]
+
+
+def remove_x(text):
+    lines = text.split("\n")
+    for i, line in enumerate(lines):
+        if line.startswith("X"):
+            lines[i] = line[1:]
+    return "\n".join(lines)
+
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})')
@@ -125,37 +150,44 @@ async def chat(interaction: discord.Interaction, prompt: str):
     """Chat with MaxPowerChat"""
     global is_generating_chat_result
     global output_text
+    marker = "### Response:"
     channel = interaction.channel
-    await interaction.response.send_message(f'Please Wait.')
+
     if is_generating_chat_result:
-        on_cancel()
-        await asyncio.sleep(10)
-        output_text = ""
+        await interaction.response.send_message(f'Please Wait For The Other Response To Be Complete!')
+        return False
+    else:
+        await interaction.response.send_message(f'Please Wait!')
 
     on_submit(prompt)
     is_generating_chat_result = True
     cmp_str = f'Compute Result'
     msg = await channel.send(cmp_str)
+    final_msg = ""
     while output_text.find("[end of text]") == -1:
         await asyncio.sleep(5)
         if output_text != "":
-            if len(output_text) > 1950:
-                msg = await channel.send(cmp_str)
-            await msg.edit(content=output_text)
+            if marker in output_text:
+                response_start = output_text.index(marker) + len(marker)
+                if len(output_text[response_start:].strip()) > 1950:
+                    msg = await channel.send(cmp_str)
+                final_msg = to_utf8_compatible(extract_text(remove_x(output_text[response_start:].strip())))
+                await msg.edit(content=final_msg)
 
-    await msg.edit(content=output_text)
+    await msg.edit(content=to_utf8_compatible(extract_text(remove_x(final_msg))))
     embed = Embed(title="Chat:",
                   description=prompt, color=0x93C54B)
     embed.add_field(name="Answer:", value=str(output_text), inline=False)
     # await msg.edit(embed=embed, content=None)
     output_text = ""
     is_generating_chat_result = False
+    await channel.send("Response completed!")
 
 # Save the config to a file when the application is closed
 with open("config.json", "w") as f:
     json.dump(config, f)
-    
-    
+
+
 client.run(TOKEN)
 
 
